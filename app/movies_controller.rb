385e1c865e54b7e266b33932a307756e5bfeec25
Nathan  Visak <nathan.visak@gmail.com>
2018-08-09 18:42:29 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -49,7 +49,9 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  movie = Movie.first
+  binding.pry
+
 end
 
 def can_find_by_multiple_attributes
